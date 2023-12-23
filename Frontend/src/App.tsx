e48cc3f5a2a5d64a8f354ca22bf9e890d9ff541d
abii225 <abhayv225@gmail.com>
2023-12-23 17:04:26 +0530

    backend and interview page on progress

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,13 @@
-
-import './App.css'
-import {Navbar} from "./Components/Navbar";
+import { Navbar } from "./Components/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
-
   return (
-    <div >
-     <Navbar/> 
-
+    <div>
+      <Navbar />
+      <AllRoutes />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
